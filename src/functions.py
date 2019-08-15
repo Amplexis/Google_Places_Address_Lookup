e302e584f67389d9634d3793d448b02801137a54
Amplexis <kevin.hamlin@gmail.com>
2019-08-15 11:39:58 -0400

    Refactored for readability

--- src/functions.py
+++ src/functions.py
@@ -4,11 +4,13 @@ import string
 import requests
 import json
 
+
 def create_output_file(filepath_out):
     HEADERS = ["Passcode", "Agency", "Address_1", "Address_2", "Address_3", "Address_4", "Address_5\n"]
     with open(filepath_out, 'w') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(HEADERS)
+
 
 def fill_lists(filepath_in, agencies_orig, passcodes, agencies_formatted):
     with pd.ExcelFile(filepath_in) as xlsx:
@@ -22,6 +24,7 @@ def fill_lists(filepath_in, agencies_orig, passcodes, agencies_formatted):
             agencies_formatted.append(agency)
         result = (agencies_orig, passcodes, agencies_formatted)
         return result
+
 
 def format_agnecy_for_query(agency):
     agency = agency.translate(str.maketrans('', '', string.punctuation))
@@ -55,6 +58,7 @@ def api_call(agency):
         return result
     except Exception as e:
         print(e)
+
 
 def write_row_to_outfile(row, filepath_out):
     with open(filepath_out, 'a') as outfile:
