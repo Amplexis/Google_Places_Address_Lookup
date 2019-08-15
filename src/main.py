a504ddc2124f1c8fd98f906f6b0d2e763368581c
Amplexis <kevin.hamlin@gmail.com>
2019-08-14 21:18:35 -0400

    Append each agency to agencies list

--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ base_dir = os.path.dirname(os.path.dirname(__file__))
 filepath_in = os.path.join(base_dir, "input/", infile)
 filepath_out = os.path.join(base_dir, "output/", outfile)
 
+agencies = []
 with pd.ExcelFile(filepath_in) as xlsx:
     sample = pd.read_excel(xlsx)
 
     for i in sample.index:
-        agency = (sample["Agency"][i])
+        agencies.append(sample["Agency"][i])
