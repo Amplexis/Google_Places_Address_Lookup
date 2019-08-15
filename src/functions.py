a9f6699d96b2c2c28d7928748f4e13c970a487a1
Amplexis <kevin.hamlin@gmail.com>
2019-08-15 11:14:13 -0400

    Added functions to fill lists and format agency name for query

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,7 @@
 import pathlib
 import csv
+import pandas as pd
+import string
 
 def create_output_file(filepath_out):
     if not pathlib.Path(filepath_out):
@@ -7,3 +9,21 @@ def create_output_file(filepath_out):
         with open(filepath_out, 'w') as outfile:
             writer = csv.writer(outfile)
             writer.writerow(HEADERS)
+
+def fill_lists(filepath_in, agencies_orig, passcodes, agencies_formatted):
+    with pd.ExcelFile(filepath_in) as xlsx:
+        sample = pd.read_excel(xlsx)
+
+        for i in sample.index:
+            agencies_orig.append(sample["Agency"][i])
+            passcodes.append(sample["Passcode"][i])
+            agency = sample["Agency"][i]
+            agency = format_agnecy_for_query(agency)
+            agencies_formatted.append(agency)
+        result = (agencies_orig, passcodes, agencies_formatted)
+        return result
+
+def format_agnecy_for_query(agency):
+    agency = agency.translate(str.maketrans('', '', string.punctuation))
+    agency = agency.replace(" ", "%20")
+    return agency
