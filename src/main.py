d867d27333873499df1d8a2d9ae77568d644afc2
Amplexis <kevin.hamlin@gmail.com>
2019-08-14 21:17:05 -0400

    Import sample file and extract agency name

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 
 infile = "sample_file.xlsx"
 outfile = "output.csv"
@@ -8,4 +8,8 @@ base_dir = os.path.dirname(os.path.dirname(__file__))
 filepath_in = os.path.join(base_dir, "input/", infile)
 filepath_out = os.path.join(base_dir, "output/", outfile)
 
+with pd.ExcelFile(filepath_in) as xlsx:
+    sample = pd.read_excel(xlsx)
 
+    for i in sample.index:
+        agency = (sample["Agency"][i])
