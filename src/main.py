1b4b29310160416389ff84034664dd3e46c03b98
Amplexis <kevin.hamlin@gmail.com>
2019-08-19 19:22:12 -0400

    Testing building a dataframe to write to xlsx

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 from src.functions import create_output_file, fill_lists, build_row_and_write_to_outfile
 
+import pandas as pd
+
+
 infile = "sample_file.xlsx"
 outfile = "output.csv"
 
@@ -15,6 +18,15 @@ agencies_formatted = []
 passcodes = []
 
 agencies_orig, passcodes, agencies_formatted = fill_lists(filepath_in, agencies_orig, passcodes, agencies_formatted)
-build_row_and_write_to_outfile(agencies_formatted, agencies_orig, passcodes, filepath_out)
-
-print("finished")
+df = pd.DataFrame(columns=["Passcode", "Agency", "Address_1", "Address_2"])
+for i in range(0, len(passcodes)):
+    pcode = passcodes[i]
+    agency = agencies_orig[i]
+    df = df.append({"Passcode":pcode, "Agency":agency}, ignore_index=True)
+print(df)
+writer = pd.ExcelWriter('test_output.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name="Sheet1", index=False)
+writer.save()
+# build_row_and_write_to_outfile(agencies_formatted, agencies_orig, passcodes, filepath_out)
+#
+# print("finished")
